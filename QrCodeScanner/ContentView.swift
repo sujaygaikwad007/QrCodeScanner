d14cdbb9fc259bf25734540f11c92b8c8d3ff4f7
Aniket Patil <aniket.p@reapmind.com>
2024-06-28 11:09:02 +0530

    Initial Commit

---
+++ QrCodeScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QrCodeScanner
+//
+//  Created by Aniket Patil on 28/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QrCodeScanner/QrCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QrCodeScannerApp.swift
+//  QrCodeScanner
+//
+//  Created by Aniket Patil on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct QrCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
